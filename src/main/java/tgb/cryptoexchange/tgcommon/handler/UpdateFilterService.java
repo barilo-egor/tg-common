365939983323d3ab70043401e6e194840ae62a00
barilo <e.barilo@isida.by>
2025-09-02 13:16:58 +0300

    удаление UpdateFilterService.java

--- src/main/java/tgb/cryptoexchange/tgcommon/handler/UpdateFilterService.java
+++
@@ -1,9 +0,0 @@
-package tgb.cryptoexchange.tgcommon.handler;
-
-import org.telegram.telegrambots.meta.api.objects.Update;
-import tgb.cryptoexchange.tgcommon.constants.UpdateFilterType;
-
-public interface UpdateFilterService {
-
-    UpdateFilterType getType(Update update);
-}
