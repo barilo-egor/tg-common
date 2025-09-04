80d0dd4198dc80adc7dbfa480c1a64ccf94205b0
barilo <e.barilo@isida.by>
2025-09-04 18:22:18 +0300

    TelegramUpdateEventListenerTest

--- src/test/java/tgb/cryptoexchange/tgcommon/bot/TelegramUpdateEventListenerTest.java
+++ src/test/java/tgb/cryptoexchange/tgcommon/bot/TelegramUpdateEventListenerTest.java
@@ -1,15 +1,18 @@
 package tgb.cryptoexchange.tgcommon.bot;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.ObjectProvider;
+import org.telegram.telegrambots.meta.api.methods.botapimethods.BotApiMethodMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
 import tgb.cryptoexchange.tgcommon.constants.UpdateType;
 import tgb.cryptoexchange.tgcommon.constants.UserState;
 import tgb.cryptoexchange.tgcommon.exception.HandlerTypeNotFoundException;
@@ -21,8 +24,9 @@ import tgb.cryptoexchange.tgcommon.service.sender.ResponseSender;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TelegramUpdateEventListenerTest {
@@ -110,4 +114,329 @@ class TelegramUpdateEventListenerTest {
         ));
     }
 
+    @Test
+    @DisplayName("update(TelegramUpdateEvent event) - апдейт пользователя в бане - прекращение выполнения метода")
+    void shouldSkipIfUserIsBanned() {
+        when(bannedCacheProvider.getIfAvailable()).thenReturn(bannedCache);
+        when(antiSpamProvider.getIfAvailable()).thenReturn(antiSpam);
+        TelegramUpdateEventListener listener = new TelegramUpdateEventListener(
+                redisUserStateService, new ArrayList<>(), new ArrayList<>(),
+                emptyHandler, new ArrayList<>(), antiSpamProvider, bannedCacheProvider, responseSender
+        );
+        Long chatId = 123456789L;
+        when(bannedCache.get(chatId)).thenReturn(true);
+        Update update = new Update();
+        Message message = new Message();
+        Chat chat = new Chat();
+        chat.setId(chatId);
+        message.setChat(chat);
+        update.setMessage(message);
+        TelegramUpdateEvent event = new TelegramUpdateEvent(new Object(), update);
+        listener.update(event);
+        verify(antiSpam, times(0)).isSpam(any());
+        verify(redisUserStateService, times(0)).get(any());
+    }
+
+    @Test
+    @DisplayName("update(TelegramUpdateEvent event) - апдейт является спамом - прекращение выполнения метода")
+    void shouldSkipIfSpam() {
+        when(bannedCacheProvider.getIfAvailable()).thenReturn(bannedCache);
+        when(antiSpamProvider.getIfAvailable()).thenReturn(antiSpam);
+        TelegramUpdateEventListener listener = new TelegramUpdateEventListener(
+                redisUserStateService, new ArrayList<>(), new ArrayList<>(),
+                emptyHandler, new ArrayList<>(), antiSpamProvider, bannedCacheProvider, responseSender
+        );
+        Long chatId = 123456789L;
+        when(bannedCache.get(chatId)).thenReturn(false);
+        when(antiSpam.isSpam(chatId)).thenReturn(true);
+        Update update = new Update();
+        Message message = new Message();
+        Chat chat = new Chat();
+        chat.setId(chatId);
+        message.setChat(chat);
+        update.setMessage(message);
+        TelegramUpdateEvent event = new TelegramUpdateEvent(new Object(), update);
+        listener.update(event);
+        verify(redisUserStateService, times(0)).get(any());
+    }
+
+    @Test
+    @DisplayName("update(TelegramUpdateEvent event) - апдейт для фильтра - вызов handle у фильтра")
+    void shouldHandleFilter() {
+        when(bannedCacheProvider.getIfAvailable()).thenReturn(bannedCache);
+        when(antiSpamProvider.getIfAvailable()).thenReturn(antiSpam);
+
+        Update update = new Update();
+        List<UpdateFilter> updateFilters = new ArrayList<>();
+        UpdateFilter mockedUpdateFilter = Mockito.mock(UpdateFilter.class);
+        UpdateFilter mockedNonMatchUpdateFilter = Mockito.mock(UpdateFilter.class);
+        when(mockedNonMatchUpdateFilter.match(update)).thenReturn(false);
+        when(mockedUpdateFilter.match(update)).thenReturn(true);
+        updateFilters.add(mockedNonMatchUpdateFilter);
+        updateFilters.add(mockedUpdateFilter);
+
+        TelegramUpdateEventListener listener = new TelegramUpdateEventListener(
+                redisUserStateService, new ArrayList<>(), new ArrayList<>(),
+                emptyHandler, updateFilters, antiSpamProvider, bannedCacheProvider, responseSender
+        );
+        Long chatId = 123456789L;
+        when(bannedCache.get(chatId)).thenReturn(false);
+        when(antiSpam.isSpam(chatId)).thenReturn(false);
+        Message message = new Message();
+        Chat chat = new Chat();
+        chat.setId(chatId);
+        message.setChat(chat);
+        update.setMessage(message);
+        TelegramUpdateEvent event = new TelegramUpdateEvent(new Object(), update);
+        listener.update(event);
+        verify(redisUserStateService, times(0)).get(any());
+        verify(mockedUpdateFilter).handle(update);
+    }
+
+
+    @Test
+    @DisplayName("update(TelegramUpdateEvent event) - сохранено состояние пользователя - вызов handle у обработчика состояния")
+    void shouldHandleByStateHandler() {
+        when(bannedCacheProvider.getIfAvailable()).thenReturn(bannedCache);
+        when(antiSpamProvider.getIfAvailable()).thenReturn(antiSpam);
+
+        Update update = new Update();
+        List<StateHandler> stateHandlers = new ArrayList<>();
+        StateHandler mockedStateHandler = Mockito.mock(StateHandler.class);
+        UserState userState = () -> "someState";
+        when(mockedStateHandler.getUserState()).thenReturn(userState);
+        stateHandlers.add(mockedStateHandler);
+
+        TelegramUpdateEventListener listener = new TelegramUpdateEventListener(
+                redisUserStateService, new ArrayList<>(), stateHandlers,
+                emptyHandler, new ArrayList<>(), antiSpamProvider, bannedCacheProvider, responseSender
+        );
+        Long chatId = 123456789L;
+        when(bannedCache.get(chatId)).thenReturn(false);
+        when(antiSpam.isSpam(chatId)).thenReturn(false);
+
+        when(redisUserStateService.get(chatId)).thenReturn(userState);
+        Message message = new Message();
+        Chat chat = new Chat();
+        chat.setId(chatId);
+        message.setChat(chat);
+        update.setMessage(message);
+        TelegramUpdateEvent event = new TelegramUpdateEvent(new Object(), update);
+        listener.update(event);
+        verify(mockedStateHandler).handle(update);
+    }
+
+    @Test
+    @DisplayName("update(TelegramUpdateEvent event) - не сохранено состояние - вызван empty обработчик")
+    void shouldHandleByEmptyHandlerIfNoUserState() {
+        when(bannedCacheProvider.getIfAvailable()).thenReturn(bannedCache);
+        when(antiSpamProvider.getIfAvailable()).thenReturn(antiSpam);
+
+        Update update = new Update();
+
+        TelegramUpdateEventListener listener = new TelegramUpdateEventListener(
+                redisUserStateService, new ArrayList<>(), new ArrayList<>(),
+                emptyHandler, new ArrayList<>(), antiSpamProvider, bannedCacheProvider, responseSender
+        );
+        Long chatId = 123456789L;
+        when(bannedCache.get(chatId)).thenReturn(false);
+        when(antiSpam.isSpam(chatId)).thenReturn(false);
+
+        when(redisUserStateService.get(chatId)).thenReturn(null);
+        Message message = new Message();
+        Chat chat = new Chat();
+        chat.setId(chatId);
+        chat.setType("private");
+        message.setChat(chat);
+        update.setMessage(message);
+        TelegramUpdateEvent event = new TelegramUpdateEvent(new Object(), update);
+        listener.update(event);
+        verify(emptyHandler).getEmptyMessage(chatId);
+    }
+
+    @Test
+    @DisplayName("update(TelegramUpdateEvent event) - отсутствует обработчик для состояния - вызван empty обработчик")
+    void shouldHandleByEmptyHandlerIfNoStateHandler() {
+        when(bannedCacheProvider.getIfAvailable()).thenReturn(bannedCache);
+        when(antiSpamProvider.getIfAvailable()).thenReturn(antiSpam);
+
+        Update update = new Update();
+
+        TelegramUpdateEventListener listener = new TelegramUpdateEventListener(
+                redisUserStateService, new ArrayList<>(), new ArrayList<>(),
+                emptyHandler, new ArrayList<>(), antiSpamProvider, bannedCacheProvider, responseSender
+        );
+        Long chatId = 123456789L;
+        when(bannedCache.get(chatId)).thenReturn(false);
+        when(antiSpam.isSpam(chatId)).thenReturn(false);
+
+        UserState userState = () -> "SOME_STATE";
+        when(redisUserStateService.get(chatId)).thenReturn(userState);
+        Message message = new Message();
+        Chat chat = new Chat();
+        chat.setId(chatId);
+        chat.setType("private");
+        message.setChat(chat);
+        update.setMessage(message);
+        TelegramUpdateEvent event = new TelegramUpdateEvent(new Object(), update);
+        listener.update(event);
+        verify(emptyHandler).getEmptyMessage(chatId);
+    }
+
+    @Test
+    @DisplayName("update(TelegramUpdateEvent event) - не обрабатываемый обработчиком состояния тип апдейта - вызван empty обработчик")
+    void shouldHandleByEmptyHandlerIfNotStateUpdateType() {
+        when(bannedCacheProvider.getIfAvailable()).thenReturn(bannedCache);
+        when(antiSpamProvider.getIfAvailable()).thenReturn(antiSpam);
+
+        Update update = new Update();
+
+        TelegramUpdateEventListener listener = new TelegramUpdateEventListener(
+                redisUserStateService, new ArrayList<>(), new ArrayList<>(),
+                emptyHandler, new ArrayList<>(), antiSpamProvider, bannedCacheProvider, responseSender
+        );
+        Long chatId = 123456789L;
+        when(bannedCache.get(chatId)).thenReturn(false);
+        when(antiSpam.isSpam(chatId)).thenReturn(false);
+        BotApiMethodMessage botApiMethodMessage = new SendMessage();
+        when(emptyHandler.getEmptyMessage(chatId)).thenReturn(botApiMethodMessage);
+
+        Message message = new Message();
+        Chat chat = new Chat();
+        chat.setId(chatId);
+        chat.setType("private");
+        message.setChat(chat);
+        update.setChannelPost(message);
+        TelegramUpdateEvent event = new TelegramUpdateEvent(new Object(), update);
+        listener.update(event);
+        verify(emptyHandler).getEmptyMessage(chatId);
+        verify(responseSender).execute(botApiMethodMessage);
+    }
+
+    @Test
+    @DisplayName("update(TelegramUpdateEvent event) - апдейт для обработчика апдейта - обработка передана на обработчик")
+    void shouldHandleByUpdateHandler() {
+        when(bannedCacheProvider.getIfAvailable()).thenReturn(bannedCache);
+        when(antiSpamProvider.getIfAvailable()).thenReturn(antiSpam);
+
+        Update update = new Update();
+        List<UpdateHandler> updateHandlers = new ArrayList<>();
+        UpdateHandler mockedUpdateHandler = Mockito.mock(UpdateHandler.class);
+        when(mockedUpdateHandler.getUpdateType()).thenReturn(UpdateType.MESSAGE);
+        when(mockedUpdateHandler.handle(update)).thenReturn(true);
+        updateHandlers.add(mockedUpdateHandler);
+
+        TelegramUpdateEventListener listener = new TelegramUpdateEventListener(
+                redisUserStateService, updateHandlers, new ArrayList<>(),
+                emptyHandler, new ArrayList<>(), antiSpamProvider, bannedCacheProvider, responseSender
+        );
+        Long chatId = 123456789L;
+        when(bannedCache.get(chatId)).thenReturn(false);
+        when(antiSpam.isSpam(chatId)).thenReturn(false);
+
+        Message message = new Message();
+        Chat chat = new Chat();
+        chat.setId(chatId);
+        message.setChat(chat);
+        update.setMessage(message);
+        TelegramUpdateEvent event = new TelegramUpdateEvent(new Object(), update);
+        listener.update(event);
+        verify(mockedUpdateHandler).handle(update);
+    }
+
+    @Test
+    @DisplayName("update(TelegramUpdateEvent event) - проброс исключения - отправка сообщения пользователю об ошибке")
+    void shouldSendMessageIfExceptionThrown() {
+        when(bannedCacheProvider.getIfAvailable()).thenReturn(bannedCache);
+        when(antiSpamProvider.getIfAvailable()).thenReturn(antiSpam);
+        TelegramUpdateEventListener listener = new TelegramUpdateEventListener(
+                redisUserStateService, new ArrayList<>(), new ArrayList<>(),
+                emptyHandler, new ArrayList<>(), antiSpamProvider, bannedCacheProvider, responseSender
+        );
+
+        Update update = new Update();
+        Long chatId = 123456789L;
+        Message message = new Message();
+        Chat chat = new Chat();
+        chat.setId(chatId);
+        message.setChat(chat);
+        update.setMessage(message);
+        TelegramUpdateEvent event = new TelegramUpdateEvent(new Object(), update);
+        when(bannedCache.get(chatId)).thenThrow(NullPointerException.class);
+        listener.update(event);
+        ArgumentCaptor<String> messageCaptor = ArgumentCaptor.forClass(String.class);
+        verify(responseSender).sendMessage(eq(chatId), messageCaptor.capture());
+        String value = messageCaptor.getValue();
+        assertAll(
+                () -> assertTrue(value.startsWith("Произошла ошибка.")),
+                () -> assertTrue(value.endsWith("Введите /start для выхода в главное меню."))
+        );
+    }
+
+    @Test
+    @DisplayName("update(TelegramUpdateEvent event) - не обрабатываемый обработчиком состояния тип апдейта - вызван empty обработчик, message не отправлен")
+    void shouldHandleByEmptyHandlerWithoutSend() {
+        when(bannedCacheProvider.getIfAvailable()).thenReturn(bannedCache);
+        when(antiSpamProvider.getIfAvailable()).thenReturn(antiSpam);
+
+        Update update = new Update();
+
+        TelegramUpdateEventListener listener = new TelegramUpdateEventListener(
+                redisUserStateService, new ArrayList<>(), new ArrayList<>(),
+                emptyHandler, new ArrayList<>(), antiSpamProvider, bannedCacheProvider, responseSender
+        );
+        Long chatId = 123456789L;
+        when(bannedCache.get(chatId)).thenReturn(false);
+        when(antiSpam.isSpam(chatId)).thenReturn(false);
+        when(emptyHandler.getEmptyMessage(chatId)).thenReturn(null);
+
+        Message message = new Message();
+        Chat chat = new Chat();
+        chat.setId(chatId);
+        chat.setType("private");
+        message.setChat(chat);
+        update.setChannelPost(message);
+        TelegramUpdateEvent event = new TelegramUpdateEvent(new Object(), update);
+        listener.update(event);
+        verify(emptyHandler).getEmptyMessage(chatId);
+        verify(responseSender, times(0)).execute(any());
+    }
+
+    @Test
+    @DisplayName("update(TelegramUpdateEvent event) - поступление апдейтов во время обработки другого апдейта - должен пропустить обработку новых апдейтов")
+    void shouldSkipUpdatesIfOneAlreadyProcessing() {
+        when(bannedCacheProvider.getIfAvailable()).thenReturn(bannedCache);
+        when(antiSpamProvider.getIfAvailable()).thenReturn(antiSpam);
+
+        TelegramUpdateEventListener listener = new TelegramUpdateEventListener(
+                redisUserStateService, new ArrayList<>(), new ArrayList<>(),
+                emptyHandler, new ArrayList<>(), antiSpamProvider, bannedCacheProvider, responseSender
+        );
+        long chatId = 123456789L;
+        Update update = new Update();
+        Message message = new Message();
+        Chat chat = new Chat();
+        chat.setId(chatId);
+        chat.setType("private");
+        message.setChat(chat);
+        update.setMessage(message);
+        TelegramUpdateEvent event = new TelegramUpdateEvent(new Object(), update);
+
+        when(bannedCache.get(chatId)).thenAnswer(invocationOnMock -> {
+            Thread.sleep(5000);
+            return true;
+        });
+        listener.update(event);
+        for (int i = 0; i < 5; i++) {
+            Update skipUpdate = new Update();
+            Message skipUpdateMessage = new Message();
+            Chat skipUpdateChat = new Chat();
+            skipUpdateChat.setId(chatId + i + 1);
+            skipUpdateChat.setType("private");
+            skipUpdateMessage.setChat(skipUpdateChat);
+            skipUpdate.setMessage(skipUpdateMessage);
+            listener.update(new TelegramUpdateEvent(new Object(), skipUpdate));
+        }
+        verify(bannedCache).get(chatId);
+    }
 }
